e958cf94ff05e2f211023e9fdf0478b74cf8cd66
volo h <vhavrylyak.it@gmail.com>
2022-08-10 19:16:27 +0300

    TEMP - move code from handler i routes TO main.go
    go run ./cmd/api/main.go
    # command-line-arguments
    cmd/api/main.go:21:16: app.routes undefined (type Config has no field or method routes)

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 const webPort = "80"
@@ -26,4 +31,42 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/", app.Broker)
+
+	return mux
+}
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(out)
 }
